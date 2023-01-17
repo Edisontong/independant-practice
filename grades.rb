6f1a23a1a31bcfa441c97f9741ea6a878885c17c
Edisontong <edisonjtong@gmail.com>
2023-01-16 21:02:26 -0700

    added code to determine grades

--- grades.rb
+++ grades.rb
@@ -1,6 +1,23 @@
 def final_grade(array)
-
-  # This method currently has no code.
-  # Your job is to write the code to get the method to work!
-
+  index = 0
+  total = 0
+  while index < array.length
+    total = total + array[index]
+    index += 1
+  end
+  average = total / array.length
+  if average >= 90
+    grade = "A"
+  elsif average >= 80
+    grade = "B"
+  elsif average >= 70
+    grade = "C"
+  elsif average >= 60
+    grades = "D"
+  elsif average < 60
+    grades = "F"
+  end
+  p grade
 end
+
+final_grade([90.0, 85.0, 70.0, 80.0])
