319edd8557f2dbdd645de546fe09784c7e3c22cf
Edisontong <edisonjtong@gmail.com>
2023-01-17 09:38:02 -0700

    added conditionals for bobs responses

--- bob.rb
+++ bob.rb
@@ -1,3 +1,19 @@
 def bob_responds(sentence)
-  # Your job is to write the code for this method!
+  if sentence == sentence.upcase && sentence.end_with?("?")
+    puts "Calm down, I know what I'm doing!"
+  elsif sentence == sentence.upcase
+    puts "Why are you yelling at me?"
+  elsif sentence.end_with?(".")
+    puts "Sounds good."
+  elsif sentence.end_with?("!")
+    puts "Whoa chill out!"
+  elsif sentence.end_with?("?")
+    puts "Sure"
+  elsif sentence.include?(" ")
+    puts "Fine, be that way."
+  else
+    puts "Whatever"
+  end
 end
+
+bob_responds("HELLo")
